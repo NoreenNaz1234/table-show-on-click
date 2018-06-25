d2e7816c2c3b3de6157d70baf0e847f88741ac80
Noreennaz <chahatchahat725@gmail.com>
2018-06-25 05:41:04 +0500

    My first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// function table() {
+// 	let userInput = parseInt(document.queryselector("input").value);
+// 	let para = document.queryselector("P");
+// 	para.innerHTML= 2 * 2;
+// }
+
+var num = 5;
+for(var i = 1; i >=10; i++){
+
+	document.write( "<br>", num," * ",i," = " ,num*i ); 
+} 
